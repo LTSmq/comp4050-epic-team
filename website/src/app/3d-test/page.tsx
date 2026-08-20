e0c221844d4ea75748f5b957721debec5765c5cd
LTSmq <lachlan.thomassmith@students.mq.edu.au>
2026-08-21 09:30:43 +1000

    Modified camera initialization

--- website/src/app/3d-test/page.tsx
+++ website/src/app/3d-test/page.tsx
@@ -18,7 +18,8 @@ const packingSolution: Item[] = [
     { uuid: "item7", position: { x: 0.75, y: 0.2, z: 0.5 },  size: { x: 0.25, y: 0.8, z: 0.5 } },
 ]
 
-const boxSize = { x: 1.0, y: 1.0, z: 1.0 }
+let boxSize = { x: 1.0, y: 1.0, z: 1.0 }
+let center = { x: boxSize.x / 2.0, y: boxSize.y / 2.0, z: boxSize.z / 2.0 }
 
 export default function Page() {
     const [itemIndex, setItemIndex] = useState(1)
@@ -30,10 +31,12 @@ export default function Page() {
     return <div style={containerStyle}>
         <button onClick={() => {incrementSolution(+1)}}>NEXT ITEM</button>
         <button onClick={() => {incrementSolution(-1)}}>PREVIOUS ITEM</button>
-        <Canvas style={canvasStyle} >
+        <Canvas style={canvasStyle} camera={{position: [0.0, center.y, boxSize.z * 3.0]}}>
             <ambientLight intensity={2.0}/>
+            <OrbitControls enableDamping target={[center.x, center.y, center.z]}/>
+
+            {/* 🡓 This is the primary component of the page implemented in @/componenets/boxViewPanel/boxViewPanel.tsx 🡓 */}
             <PackageDisplay items={packingSolution.slice(0, itemIndex)} size={boxSize} ghostAnimationTime={2.5} />
-            <OrbitControls enableDamping />
         </Canvas>
     </div>
 }
